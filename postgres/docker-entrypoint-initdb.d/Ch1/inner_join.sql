9e6bc06247968b3f33c9f16f1fe6647650dab5dc
ryuji0123 <ryo1011.javelin@gmail.com>
2023-01-28 17:42:49 +0900

    Add inner_join.sql

---
+++ postgres/docker-entrypoint-initdb.d/Ch1/inner_join.sql
@@ -0,0 +1,8 @@
+EXPLAIN
+    SELECT shop_name
+    FROM Shops S INNER JOIN Reservations R
+        ON S.shop_id = R.shop_id;
+
+SELECT shop_name
+FROM Shops S INNER JOIN Reservations R
+    ON S.shop_id = R.shop_id;
