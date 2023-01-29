f8fc1fc5c563f2e09e06db5f5a2f7d84626dca40
ryuji0123 <ryo1011.javelin@gmail.com>
2023-01-30 00:34:31 +0900

    Add selecting_from_converted_address.sql

---
+++ postgres/docker-entrypoint-initdb.d/Ch2/selecting_from_converted_address.sql
@@ -0,0 +1,8 @@
+SELECT name, address,
+    CASE WHEN address = '東京都' THEN '関東'
+        WHEN address = '千葉県' THEN '関東'
+        WHEN address = '福島県' THEN '東北'
+        WHEN address = '三重県' THEN '中部'
+        WHEN address = '和歌山県' THEN '関西'
+        ELSE NULL END AS district
+    FROM Address;
