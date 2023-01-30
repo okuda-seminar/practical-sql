9c5c9b9990a4efeb10f13f09ca575f76b10c41f2
ryuji0123 <ryo1011.javelin@gmail.com>
2023-01-30 21:35:39 +0900

    Update updating_multiple_columns.sql

--- postgres/docker-entrypoint-initdb.d/Ch2/updating_multiple_columns.sql
+++ postgres/docker-entrypoint-initdb.d/Ch2/updating_multiple_columns.sql
@@ -1,4 +1,8 @@
+-- UPDATE Address
+--     SET phone_nbr = '080-5848-XXXX',
+--         age = 20
+-- WHERE name = '佐々木';
+
 UPDATE Address
-    SET phone_nbr = '080-5848-XXXX',
-        age = 20
+    SET (phone_nbr, age) = ('080-5849-XXXX', 19)
 WHERE name = '佐々木';
