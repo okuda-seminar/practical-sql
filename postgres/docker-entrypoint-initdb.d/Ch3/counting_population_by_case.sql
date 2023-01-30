ca1b418dd1a13d2ab889459b87c8cdce009d1b67
ryuji0123 <ryo1011.javelin@gmail.com>
2023-01-30 21:55:42 +0900

    Add counting_population_by_case.sql

---
+++ postgres/docker-entrypoint-initdb.d/Ch3/counting_population_by_case.sql
@@ -0,0 +1,5 @@
+SELECT prefecture,
+    SUM(CASE WHEN sex = '1' THEN pop ELSE 0 END) AS pop_men,
+    SUM(CASE WHEN sex = '2' THEN pop ELSE 0 END) AS pop_wom
+    FROM Population
+GROUP BY prefecture;
