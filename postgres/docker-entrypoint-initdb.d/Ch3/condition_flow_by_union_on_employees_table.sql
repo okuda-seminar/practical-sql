4c009a3085c28e72870d36a5d06557701090b3c6
ryuji0123 <ryo1011.javelin@gmail.com>
2023-01-30 22:08:56 +0900

    Update condition_flow_by_union_on_employees_table.sql

--- postgres/docker-entrypoint-initdb.d/Ch3/condition_flow_by_union_on_employees_table.sql
+++ postgres/docker-entrypoint-initdb.d/Ch3/condition_flow_by_union_on_employees_table.sql
@@ -19,13 +19,22 @@ GROUP BY emp_name
 HAVING COUNT(*) = 1
 UNION
 SELECT emp_name,
-    MAX(team) AS team
+    'double' AS team
     FROM Employees
 GROUP BY emp_name
 HAVING COUNT(*) = 2
 UNION
 SELECT emp_name,
-    MAX(team) AS team
+    'more than triple' AS team
     FROM Employees
 GROUP BY emp_name
 HAVING COUNT(*) >= 3;
+
+SELECT emp_name,
+    CASE 
+        WHEN COUNT(*) = 1 THEN MAX(team)
+        WHEN COUNT(*) = 2 THEN 'double'
+        WHEN COUNT(*) >= 3 THEN 'more then triple'
+    END AS team
+    FROM Employees
+GROUP BY emp_name;
