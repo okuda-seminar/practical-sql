79274a410c00f83c4e48225c2293e189dc33997e
ryuji0123 <ryo1011.javelin@gmail.com>
2023-01-30 22:20:27 +0900

    Add condition_flow_by_in.sql

---
+++ postgres/docker-entrypoint-initdb.d/Ch3/condition_flow_by_in.sql
@@ -0,0 +1,12 @@
+SELECT key, name,
+    date_1, flg_1,
+    date_2, flg_2,
+    date_3, flg_3
+    FROM ThreeElements
+WHERE
+    ('2013-11-01', 'T')
+IN (
+    (date_1, flg_1),
+    (date_2, flg_2),
+    (date_3, flg_3)
+);
