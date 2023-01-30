5bfde8b50c1603d9f81935652c61a2fb96257c70
ryuji0123 <ryo1011.javelin@gmail.com>
2023-01-30 22:23:29 +0900

    Add condition_flow_by_case.sql

---
+++ postgres/docker-entrypoint-initdb.d/Ch3/condition_flow_by_case.sql
@@ -0,0 +1,12 @@
+SELECT key, name,
+    date_1, flg_1,
+    date_2, flg_2,
+    date_3, flg_3
+    FROM ThreeElements
+WHERE
+CASE
+    WHEN date_1 = '2013-11-01' THEN flg_1
+    WHEN date_2 = '2013-11-01' THEN flg_2
+    WHEN date_3 = '2013-11-01' THEN flg_3
+    ELSE NULL
+END = 'T';    
