2eb6f7b977970c72a88b70d9538840217574d6e7
kasturada1 <kazuki1.katsurada@gmail.com>
2023-02-01 16:20:10 +0900

    add creating_employees_table.sql

---
+++ postgres/docker-entrypoint-initdb.d/Ch3/creating_employees_table.sql
@@ -0,0 +1,21 @@
+CREATE TABLE Employees
+(emp_id    CHAR(3)  NOT NULL,
+ team_id   INTEGER  NOT NULL,
+ emp_name  CHAR(16) NOT NULL,
+ team      CHAR(16) NOT NULL,
+    PRIMARY KEY(emp_id, team_id));
+
+INSERT INTO Employees VALUES('201',	1,	'Joe',	'商品企画');
+INSERT INTO Employees VALUES('201',	2,	'Joe',	'開発');
+INSERT INTO Employees VALUES('201',	3,	'Joe',	'営業');
+INSERT INTO Employees VALUES('202',	2,	'Jim',	'開発');
+INSERT INTO Employees VALUES('203',	3,	'Carl',	'営業');
+INSERT INTO Employees VALUES('204',	1,	'Bree',	'商品企画');
+INSERT INTO Employees VALUES('204',	2,	'Bree',	'開発');
+INSERT INTO Employees VALUES('204',	3,	'Bree',	'営業');
+INSERT INTO Employees VALUES('204',	4,	'Bree',	'管理');
+INSERT INTO Employees VALUES('205',	1,	'Kim',	'商品企画');
+INSERT INTO Employees VALUES('205',	2,	'Kim',	'開発');
+
+SELECT *
+FROM Employees
