cbccb48afe3d87a5085ddcc1af7774e91ea0a8c5
kasturada1 <kazuki1.katsurada@gmail.com>
2023-02-01 19:42:25 +0900

    add ranking_similar_pcodes_by_window_function.sql

---
+++ postgres/docker-entrypoint-initdb.d/Ch5/ranking_similar_pcodes_by_window_function.sql
@@ -0,0 +1,91 @@
+ EXPLAIN  
+    SELECT pcode,
+        district_name
+    FROM (SELECT pcode,
+                district_name,
+                CASE WHEN pcode = '4130033' THEN 0
+                        WHEN pcode LIKE '413003%' THEN 1
+                        WHEN pcode LIKE '41300%'  THEN 2
+                        WHEN pcode LIKE '4130%'   THEN 3
+                        WHEN pcode LIKE '413%'    THEN 4
+                        WHEN pcode LIKE '41%'     THEN 5
+                        WHEN pcode LIKE '4%'      THEN 6
+                        ELSE NULL END AS hit_code,
+                MIN(CASE WHEN pcode = '4130033' THEN 0
+                            WHEN pcode LIKE '413003%' THEN 1
+                            WHEN pcode LIKE '41300%'  THEN 2
+                            WHEN pcode LIKE '4130%'   THEN 3
+                            WHEN pcode LIKE '413%'    THEN 4
+                            WHEN pcode LIKE '41%'     THEN 5
+                            WHEN pcode LIKE '4%'      THEN 6
+                            ELSE NULL END) 
+                    OVER(ORDER BY CASE WHEN pcode = '4130033' THEN 0
+                                    WHEN pcode LIKE '413003%' THEN 1
+                                    WHEN pcode LIKE '41300%'  THEN 2
+                                    WHEN pcode LIKE '4130%'   THEN 3
+                                    WHEN pcode LIKE '413%'    THEN 4
+                                    WHEN pcode LIKE '41%'     THEN 5
+                                    WHEN pcode LIKE '4%'      THEN 6
+                                    ELSE NULL END) AS min_code
+            FROM PostalCode) Foo
+    WHERE hit_code = min_code;
+
+SELECT pcode,
+       district_name
+  FROM (SELECT pcode,
+               district_name,
+               CASE WHEN pcode = '4130033' THEN 0
+                    WHEN pcode LIKE '413003%' THEN 1
+                    WHEN pcode LIKE '41300%'  THEN 2
+                    WHEN pcode LIKE '4130%'   THEN 3
+                    WHEN pcode LIKE '413%'    THEN 4
+                    WHEN pcode LIKE '41%'     THEN 5
+                    WHEN pcode LIKE '4%'      THEN 6
+                    ELSE NULL END AS hit_code,
+               MIN(CASE WHEN pcode = '4130033' THEN 0
+                        WHEN pcode LIKE '413003%' THEN 1
+                        WHEN pcode LIKE '41300%'  THEN 2
+                        WHEN pcode LIKE '4130%'   THEN 3
+                        WHEN pcode LIKE '413%'    THEN 4
+                        WHEN pcode LIKE '41%'     THEN 5
+                        WHEN pcode LIKE '4%'      THEN 6
+                        ELSE NULL END) 
+                OVER(ORDER BY CASE WHEN pcode = '4130033' THEN 0
+                                   WHEN pcode LIKE '413003%' THEN 1
+                                   WHEN pcode LIKE '41300%'  THEN 2
+                                   WHEN pcode LIKE '4130%'   THEN 3
+                                   WHEN pcode LIKE '413%'    THEN 4
+                                   WHEN pcode LIKE '41%'     THEN 5
+                                   WHEN pcode LIKE '4%'      THEN 6
+                                   ELSE NULL END) AS min_code
+          FROM PostalCode) Foo
+ WHERE hit_code = min_code;
+
+--  Foo table
+SELECT pcode,
+               district_name,
+               CASE WHEN pcode = '4130033' THEN 0
+                    WHEN pcode LIKE '413003%' THEN 1
+                    WHEN pcode LIKE '41300%'  THEN 2
+                    WHEN pcode LIKE '4130%'   THEN 3
+                    WHEN pcode LIKE '413%'    THEN 4
+                    WHEN pcode LIKE '41%'     THEN 5
+                    WHEN pcode LIKE '4%'      THEN 6
+                    ELSE NULL END AS hit_code,
+               MIN(CASE WHEN pcode = '4130033' THEN 0
+                        WHEN pcode LIKE '413003%' THEN 1
+                        WHEN pcode LIKE '41300%'  THEN 2
+                        WHEN pcode LIKE '4130%'   THEN 3
+                        WHEN pcode LIKE '413%'    THEN 4
+                        WHEN pcode LIKE '41%'     THEN 5
+                        WHEN pcode LIKE '4%'      THEN 6
+                        ELSE NULL END) 
+                OVER(ORDER BY CASE WHEN pcode = '4130033' THEN 0
+                                   WHEN pcode LIKE '413003%' THEN 1
+                                   WHEN pcode LIKE '41300%'  THEN 2
+                                   WHEN pcode LIKE '4130%'   THEN 3
+                                   WHEN pcode LIKE '413%'    THEN 4
+                                   WHEN pcode LIKE '41%'     THEN 5
+                                   WHEN pcode LIKE '4%'      THEN 6
+                                   ELSE NULL END) AS min_code
+          FROM PostalCode
