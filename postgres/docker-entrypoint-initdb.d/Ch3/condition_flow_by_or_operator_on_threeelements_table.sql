074c461b79cab11b71c3749cfd5c327a7c2cf32e
kasturada1 <kazuki1.katsurada@gmail.com>
2023-02-01 16:34:18 +0900

    add condition_flow_by_or_operator_on_threeelements_table.sql

---
+++ postgres/docker-entrypoint-initdb.d/Ch3/condition_flow_by_or_operator_on_threeelements_table.sql
@@ -0,0 +1,8 @@
+SELECT key, name,
+       date_1, flg_1,
+       date_2, flg_2,
+       date_3, flg_3
+  FROM ThreeElements
+ WHERE (date_1 = '2013-11-01' AND flg_1 = 'T')
+    OR (date_2 = '2013-11-01' AND flg_2 = 'T')
+    OR (date_3 = '2013-11-01' AND flg_3 = 'T');
