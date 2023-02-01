266fd031df87bd1d6ce6490633b1a7f8a14695f5
kasturada1 <kazuki1.katsurada@gmail.com>
2023-02-01 19:58:26 +0900

    add selecting_oldest_pcode_by_nested_model.sql

---
+++ postgres/docker-entrypoint-initdb.d/Ch5/selecting_oldest_pcode_by_nested_model.sql
@@ -0,0 +1,8 @@
+SELECT name, pcode
+  FROM PostalHistory2 PH1
+ WHERE name = 'A'
+   AND NOT EXISTS 
+        (SELECT *
+           FROM PostalHistory2 PH2
+          WHERE PH2.name = 'A'
+            AND PH1.lft > PH2.lft);
