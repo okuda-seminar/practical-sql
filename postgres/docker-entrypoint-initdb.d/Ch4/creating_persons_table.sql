479b4941e11622ec7456f8178daff65f8c0415ad
kasturada1 <kazuki1.katsurada@gmail.com>
2023-02-01 18:31:52 +0900

    add creating_persons_table.sql

---
+++ postgres/docker-entrypoint-initdb.d/Ch4/creating_persons_table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Persons
+(name   VARCHAR(8) NOT NULL,
+ age    INTEGER NOT NULL,
+ height FLOAT NOT NULL,
+ weight FLOAT NOT NULL,
+ PRIMARY KEY (name));
+
+
+INSERT INTO Persons VALUES('Anderson',  30,  188,  90);
+INSERT INTO Persons VALUES('Adela',    21,  167,  55);
+INSERT INTO Persons VALUES('Bates',    87,  158,  48);
+INSERT INTO Persons VALUES('Becky',    54,  187,  70);
+INSERT INTO Persons VALUES('Bill',    39,  177,  120);
+INSERT INTO Persons VALUES('Chris',    90,  175,  48);
+INSERT INTO Persons VALUES('Darwin',  12,  160,  55);
+INSERT INTO Persons VALUES('Dawson',  25,  182,  90);
+INSERT INTO Persons VALUES('Donald',  30,  176,  53);
