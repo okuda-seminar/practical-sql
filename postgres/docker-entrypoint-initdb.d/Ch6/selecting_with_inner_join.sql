52a9c83cdae87df86d0e367554adcbc6e6cca461
ryuji0123 <ryo1011.javelin@gmail.com>
2023-02-03 00:05:20 +0900

    Update selecting_with_inner_join.sql

--- postgres/docker-entrypoint-initdb.d/Ch6/selecting_with_inner_join.sql
+++ postgres/docker-entrypoint-initdb.d/Ch6/selecting_with_inner_join.sql
@@ -1,4 +1,13 @@
 SELECT E.emp_id, E.emp_name, E.dept_id, D.dept_name
     FROM Employees2 E
 INNER JOIN Departments D
-    ON E.dept_id = D.dept_id;    
+    ON E.dept_id = D.dept_id;
+
+SELECT E.emp_id, E.emp_name, E.dept_id,
+    (
+        SELECT D.dept_name
+            FROM Departments D
+        WHERE E.dept_id = D.dept_id
+    )
+    AS dept_name
+    FROM Employees2 E;
