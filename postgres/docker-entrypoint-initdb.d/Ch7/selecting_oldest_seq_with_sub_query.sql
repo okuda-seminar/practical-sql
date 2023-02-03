73d1436bac86a5b9187ea4edb76f66e078ed09d3
ryuji0123 <ryo1011.javelin@gmail.com>
2023-02-03 22:38:28 +0900

    Add selecting_oldest_seq_with_sub_query.sql

---
+++ postgres/docker-entrypoint-initdb.d/Ch7/selecting_oldest_seq_with_sub_query.sql
@@ -0,0 +1,10 @@
+SELECT R1.cust_id, R1.seq, R1.price
+    FROM Receipts R1
+INNER JOIN (
+    SELECT cust_id, MIN(seq) AS min_seq
+        FROM Receipts
+    GROUP BY cust_id
+) R2
+ON 
+    R1.cust_id = R2.cust_id AND
+    R1.seq = R2.min_seq;
