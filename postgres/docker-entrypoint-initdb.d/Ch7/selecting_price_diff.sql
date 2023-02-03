829e26377b8c1ebdff16b46a77a3dbf1dbbad107
ryuji0123 <ryo1011.javelin@gmail.com>
2023-02-03 22:57:45 +0900

    Add selecting_price_diff.sql

---
+++ postgres/docker-entrypoint-initdb.d/Ch7/selecting_price_diff.sql
@@ -0,0 +1,26 @@
+SELECT cust_id, 
+    SUM(
+        CASE WHEN min_seq = 1 THEN price ELSE 0 END
+    )
+    -
+    SUM(
+        CASE WHEN max_seq = 1 THEN price ELSE 0 END
+    )
+    AS diff
+    FROM (
+        SELECT cust_id, price,
+            ROW_NUMBER() OVER (
+                PARTITION BY cust_id ORDER BY seq
+            )
+            AS min_seq,
+            ROW_NUMBER() OVER (
+                PARTITION BY cust_id ORDER BY seq DESC
+            )
+            AS max_seq
+            FROM Receipts
+    ) WORK
+WHERE 
+    WORK.min_seq = 1
+OR
+    WORK.max_seq = 1
+GROUP BY cust_id;
