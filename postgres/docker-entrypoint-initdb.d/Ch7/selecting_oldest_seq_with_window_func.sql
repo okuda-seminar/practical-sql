3b9a098f91d060352f9817d5a77a66e9ab22b221
ryuji0123 <ryo1011.javelin@gmail.com>
2023-02-03 22:49:38 +0900

    Add selecting_oldest_seq_with_window_func.sql

---
+++ postgres/docker-entrypoint-initdb.d/Ch7/selecting_oldest_seq_with_window_func.sql
@@ -0,0 +1,11 @@
+SELECT cust_id, seq, price
+    FROM (
+        SELECT cust_id, seq, price,
+        ROW_NUMBER() OVER(
+            PARTITION BY cust_id
+            ORDER BY seq
+        )
+        AS row_seq
+            FROM Receipts
+    ) WORK
+WHERE WORK.row_seq = 1;
