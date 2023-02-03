d11973997dc01e1037a976f8e27cf7f9f0443916
kasturada1 <kazuki1.katsurada@gmail.com>
2023-02-03 22:49:01 +0900

    add selecting_with_triangle_join.sql

---
+++ postgres/docker-entrypoint-initdb.d/Ch6/selecting_with_triangle_join.sql
@@ -0,0 +1,14 @@
+EXPLAIN
+    SELECT A.col_a, B.col_b, C.col_c
+    FROM Table_A A
+            INNER JOIN Table_B B
+                ON A.col_a = B.col_b
+                INNER JOIN Table_C C
+                    ON A.col_a = C.col_c;
+
+SELECT A.col_a, B.col_b, C.col_c
+  FROM Table_A A
+         INNER JOIN Table_B B
+            ON A.col_a = B.col_b
+              INNER JOIN Table_C C
+                 ON A.col_a = C.col_c;
