db6f72288c00e820739e3f3d43217e18e49df60f
ryuji0123 <ryo1011.javelin@gmail.com>
2023-02-03 22:45:07 +0900

    Add selecting_oldest_seq_with_correlated_subquery.sql

---
+++ postgres/docker-entrypoint-initdb.d/Ch7/selecting_oldest_seq_with_correlated_subquery.sql
@@ -0,0 +1,16 @@
+EXPLAIN
+SELECT cust_id, seq, price
+    FROM Receipts R1
+WHERE seq = (
+    SELECT MIN(seq)
+        FROM Receipts R2
+    WHERE R1.cust_id = R2.cust_id
+);
+
+SELECT cust_id, seq, price
+    FROM Receipts R1
+WHERE seq = (
+    SELECT MIN(seq)
+        FROM Receipts R2
+    WHERE R1.cust_id = R2.cust_id
+);
