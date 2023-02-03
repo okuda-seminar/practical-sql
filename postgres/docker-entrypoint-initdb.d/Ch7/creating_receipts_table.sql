8e380af7c4f5b733fdefeb770922dbd6807dfe74
ryuji0123 <ryo1011.javelin@gmail.com>
2023-02-03 22:33:59 +0900

    Add creating_receipts_table.sql

---
+++ postgres/docker-entrypoint-initdb.d/Ch7/creating_receipts_table.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Receipts
+(
+    cust_id   CHAR(1) NOT NULL, 
+    seq   INTEGER NOT NULL, 
+    price   INTEGER NOT NULL, 
+    PRIMARY KEY (cust_id, seq)
+);
+
+INSERT INTO Receipts VALUES ('A',   1   ,500    );
+INSERT INTO Receipts VALUES ('A',   2   ,1000   );
+INSERT INTO Receipts VALUES ('A',   3   ,700    );
+INSERT INTO Receipts VALUES ('B',   5   ,100    );
+INSERT INTO Receipts VALUES ('B',   6   ,5000   );
+INSERT INTO Receipts VALUES ('B',   7   ,300    );
+INSERT INTO Receipts VALUES ('B',   9   ,200    );
+INSERT INTO Receipts VALUES ('B',   12  ,1000   );
+INSERT INTO Receipts VALUES ('C',   10  ,600    );
+INSERT INTO Receipts VALUES ('C',   20  ,100    );
+INSERT INTO Receipts VALUES ('C',   45  ,200    );
+INSERT INTO Receipts VALUES ('C',   70  ,50     );
+INSERT INTO Receipts VALUES ('D',   3   ,2000   );
+
